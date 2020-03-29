4d6211f1cc59e83f81cf2cca8bf14bdb5bebf45a
gladiator2ww <sascko@mail.ru>
2020-03-29 13:39:17 +0300

    first commit

---
+++ Hello, world!/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hello__world_
+{
+    class Program
+    {
+        public static int GetSquare(int number)
+        {
+            int result = (int)Math.Pow(number, 2);
+            return result;
+        }
+        public static void Print(int result)
+        {
+            Console.WriteLine(result);
+        }
+        public static void Main()
+        {
+            Print(GetSquare(42));
+        }
+    }
+}
